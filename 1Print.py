585705c0cbb5365559ceaf4436489b3a7e4f817d
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-04 22:51:39 -0400

    jaja

--- 1Print.py
+++ 1Print.py
@@ -2,3 +2,7 @@ print("hola")       #This doesn't need ;
 print("jajajaja")   #The println is done automatically
 
 # Only way known by me to make comments
+
+x = 12
+print(f"My number is {x}")  #You can add variables to prints like this
+                            #I don't know the other conventional method, just check it out later
