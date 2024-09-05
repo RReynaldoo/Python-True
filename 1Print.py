25cf43f58f393c72e3f15032b190c690afae25dd
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-04 22:47:53 -0400

    jajaj

---
+++ 1Print.py
@@ -0,0 +1,4 @@
+print("hola")       #This doesn't need ;
+print("jajajaja")   #The println is done automatically
+
+# Only way known by me to make comments
---
+++ 2Variables.py
@@ -0,0 +1,9 @@
+#Variables = no need to specify the data type, just assign them
+
+a = 12          #Integers
+
+b = 12.99       #floats
+
+c = True        #booleans
+
+d = "anything"  #strings
