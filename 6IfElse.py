c8c6c358b57bc0071fcd680cd03364d8673bb0a3
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-05 16:36:31 -0400

    ja

---
+++ 6IfElse.py
@@ -0,0 +1,19 @@
+#if else statement = no curly brackets, you indicate what executes with indentation
+
+age = 12
+
+if age > 12:
+    print("something")
+elif age < 12:
+    print("something")
+else:
+    print("something")
+
+#Booleans
+
+condition = True
+
+if condition:           #No need for explicitly ask if it is true or not
+    print("Something")
+else:
+    print()
