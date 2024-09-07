dc824be9e2d42ceffeb93dc8a4b57d128681d3d5
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-06 22:24:15 -0400

    JAJA

--- 1Print.py
+++ 1Print.py
@@ -6,3 +6,5 @@ print("jajajaja")   #The println is done automatically
 x = 12
 print(f"My number is {x}")  #You can add variables to prints like this , remember the f before the " "
                             #I don't know the other conventional method, just check it out later
+
+print(end="")   #End makes the end of the print to end with a character or whatever we put there. AVOIDS THE NEW LINE, USEFUL FOR NESTED LOOPS
