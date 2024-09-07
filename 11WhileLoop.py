fd2875dc74c4b3f1075279820181d6e28358b178
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-06 21:39:12 -0400

    While

---
+++ 11WhileLoop.py
@@ -0,0 +1,6 @@
+#While loop = execute some code while some condition remains true
+
+condition = True
+
+while condition:                #You can't do the do while, there is only this format of doing things
+    print("Help me out pliz")
