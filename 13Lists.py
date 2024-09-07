2804f4695cef832f203d98e688e2724e45e5340b
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-06 23:04:30 -0400

    changes

--- 13Lists.py
+++ 13Lists.py
@@ -7,15 +7,17 @@
 
 fruit = ["banana", "orange", "coconut"]
 
-#fruit[1] = "Nothingness"                #Can add like normally would
-#fruit[1] ,fruit[0:2], fruit[::-1]       #Can iterate through them as with range and indexes in strings
+fruit[1] = "Nothingness"                #Can add like normally would
+fruit[1] ,fruit[0:2], fruit[::-1]       #Can iterate through them as with range and indexes in strings
 
-#dir(fruit) / help(fruit)    #shows useful stuff
-#fruit.append()              #Adds an element at the end of a list
-#fruit.remove()              #Removes an element from the list
+len(fruit)                  #For the length of the list
+dir(fruit) / help(fruit)    #shows useful stuff
+fruit.append()              #Adds an element at the end of a list
+fruit.remove()              #Removes an element from the list
 fruit.insert(1,"Apple")     #Inserts something in a position
 fruit.sort()                 #Sorts in alphabetical, or number depending on the datatype
 fruit.reverse()              
 fruit.clear()
 fruit.index("banana")        #Returns the index of an item in the list
 fruit.count("banana")        #Returns the number of times this item is repeated
+("banana" in fruit )         #Checks if an element exists
