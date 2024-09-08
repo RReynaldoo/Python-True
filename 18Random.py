d27c046958b310f850aa27a9210a201fd342b9fb
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-09-08 14:36:41 -0400

    functions

---
+++ 18Random.py
@@ -0,0 +1,7 @@
+import random
+
+random.randint(1,6)   #Generates a random integer between 1, 6 / can be replaced by variables
+random.random()             #Generates a random floating point number
+random.choice()             #Selects a random item in a collection
+random.shuffle()            #To shuffle a collection
+#And many more
---
+++ 19Functions.py
@@ -0,0 +1,21 @@
+#function = A block of reusable code
+#           place () after the function name to invoke it
+
+def happy_birthday(name,age):           #Because it does not specify the datatype we have to be careful with the order of the data we send to them
+    print(f"Happy birthday to {name}")
+    print(f"You are {age} old")
+    print(f"Happy birthday to {name}")
+    print()
+
+happy_birthday("Nick", 12)
+
+#---------------------------------------------------
+#return =   statement used to end a function
+#           and send a result black to the caller
+
+def add(x,y):
+    z = x+y
+    return z
+
+print(add(2,3))
+
